021ea2b4d076f0f7e2e1df9efe6f9fad8dcea926
2ndborn <>
2023-10-08 17:21:09 +0000

    Added the random numbers to the slots

---
+++ assets/js/game.js
@@ -0,0 +1,57 @@
+/**
+ * The game needs to response to the user pressing the “Begin” button on the main game screen to starts the game.
+ * The game needs to place random numbers into the slots and then remove them after 2 seconds.
+ * The game needs to response to the user entering numbers into the slots by moving on to the next slot(when using the keyboard).
+ * The game needs to response to the user pressing the “Submit” button by checking whether the answers given match the numbers that originally appeared.
+ * If the answers match the game must inform the user that they have given the correct answer and then add one point to their score.
+ * If the answer doesn’t match the game must inform the user that they have given the wrong answer and remove a life(one of the light bulbs) from the window.
+ * After a turn is completed, the game displays a “Continue” button.
+ * The game need to response to the user pressing the “Continue” button by placing another set of random numbers into the slots.
+ * The game must response to the user’s 3rd incorrect answer by notifying them that the game is over.
+ * The game must display the “Begin” button to enable the user to play again.
+ * */
+
+$(document).ready(function() {
+    $('button').on('click', function() {
+        $(this).text('SUBMIT');
+        newGame();
+    })
+})
+
+let newGame = () => {
+    let num1 = Math.floor(Math.random() * 9) + 1;
+    let num2 = Math.floor(Math.random() * 9) + 1;
+    displayNumbers(num1, num2);
+    
+};
+
+
+function newTurn() {
+
+}
+
+function displayNumbers(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    setTimeout(function () {
+        document.getElementById('operand1').textContent = `<input type="number">`;
+        document.getElementById('operand2').textContent = "";
+    }, 2000);
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function removeLife() {
+
+}
+
+
+
+
