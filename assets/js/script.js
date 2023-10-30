f04c141e09ba629c3e730bb5e786cb0acb2d235a
2ndborn <131536159+2ndborn@users.noreply.github.com>
2023-10-30 15:27:34 +0000

    js for ham button

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function hamIcon() {
+    let menu = document.getElementById("menu2");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block"
+    }
+}
